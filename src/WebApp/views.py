ba4c6883211896f56f1aeaae235f8b12b6d08af4
Andrei Raileanu <andrei.m.raileanu@outlook.com>
2020-06-08 17:04:27 +0300

    Added view urls

--- src/WebApp/views.py
+++ src/WebApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+	return HttpResponse('Home')
+
+
+def customer(request):
+	return HttpResponse('Customer')
+
+
+def products(request):
+	return HttpResponse('Products')
